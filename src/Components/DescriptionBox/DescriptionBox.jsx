3d22cdbe4b1b35cb7ec899dc9b0c1ac9bd6c49ee
Sylvester <dlamaistifanus0@gmail.com>
2024-02-21 13:33:04 +0100

    nbmnm

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box.fade">Review (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus optio iure similique quia saepe corporis voluptate nulla reprehenderit ipsa, id fugit, eligendi repellendus ipsam impedit ut aperiam ab minima.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus pariatur nihil ratione aut porro inventore, laudantium quod rerum qui minus, dignissimos eius molestiae deleniti ipsum vel itaque voluptatum atque magnam?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus pariatur nihil ratione aut porro inventore, laudantium quod rerum qui minus, dignissimos eius molestiae deleniti ipsum vel itaque voluptatum atque magnam?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus pariatur nihil ratione aut porro inventore, laudantium quod rerum qui minus, dignissimos eius molestiae deleniti ipsum vel itaque voluptatum atque magnam?
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
